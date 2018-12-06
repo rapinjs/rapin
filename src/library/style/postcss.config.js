c7ac03ae19a20781f2344c7e081b7fee24002b95
Alex Krasny <krasaler@gmail.com>
2018-12-06 14:33:32 +0200

    Remove postcss plugin

--- src/library/style/postcss.config.js
+++ src/library/style/postcss.config.js
@@ -4,15 +4,6 @@ module.exports = function () {
     plugins: [
       require('postcss-nested')({}),
       require('postcss-import-sync')({}),
-      require('postcss-pxtorem')({
-        rootValue: 14,
-        unitPrecision: 5,
-        propList: ['*'],
-        selectorBlackList: ['body'],
-        replace: true,
-        mediaQuery: true,
-        minPixelValue: 0
-      }),
       require('postcss-font-magician')({
         protocol: 'https:',
         foundries: 'bootstrap google'
